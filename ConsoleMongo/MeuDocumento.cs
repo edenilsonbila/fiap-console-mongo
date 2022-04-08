cb06e4b895a26ee784478d65fc454c46789f8251
EdenilsonBila <edn_santos@hotmail.com>
2022-04-08 01:02:36 -0300

    Add project files.

---
+++ ConsoleMongo/MeuDocumento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ConsoleMongo
+{
+    //[BsonIgnoreExtraElements] //Ignora o que nao tiver no Elemento
+    public class MeuDocumento
+    {
+        [BsonElement("nome_completo")]//Da um nome em como vai serializar essa coluna
+        public string Nome { get; set; }
+
+        [BsonExtraElements]//Elementos que nao fazem parte da classe, mas que estejam no mongo
+        public BsonDocument Outros { get; set; }
+
+    }
+}
---
+++ ConsoleMongo/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleMongo;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+string conString = "mongodb://localhost:27017";
+var cliente = new MongoClient(conString);
+
+var db = cliente.GetDatabase("db02");
+var col = db.GetCollection<BsonDocument>("col02");
+
+//var doc = BsonDocument.Parse("{ Nome: 'Teste' }");
+
+var doc = new BsonDocument()
+{
+    {"Nome", "Edenilson" },
+    {"Aula",
+        new BsonArray(){
+        "C#", "SQL"
+        }
+    }
+};
+
+var colMeuDoc = db.GetCollection<MeuDocumento>("col02");
+var doc2 = new MeuDocumento() { Nome = "Edenilson" };
+colMeuDoc.InsertOne(doc2);
+
+col.InsertOne(doc);
+
+var filtraTudo = BsonDocument.Parse("{}");
+
+var resultado = col.Find(filtraTudo).ToList();
+
+var updateSet2 = Builders<BsonDocument>.Update.Set("Nome", "Text");
+
+var a = "pause";
